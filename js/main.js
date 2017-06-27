cdc254b547b7f1d094f82f36b9b2b33ceec07131
YessiGarcia <aria.801900@gmail.com>
2017-06-27 15:45:48 -0500

    appi

--- js/main.js
+++ js/main.js
@@ -1,48 +1,37 @@
 var api = {
-  url: 'http://laboratoria.cuadra.co:9339/api/v1/students/'
+  url: 'http://laboratoria.cuadra.co:9339/api/v1/students/ '
 };
 
 var plantillaFinal = '';
 
 var cargarPagina = function() {
-  cargarTemas();
-  $('#add-form').submit(agregarEstudiante);
+  cargarLista();
 };
 
-var cargarTemas = function() {
-  $.getJSON(api.url, function(topics) {
-    topics.forEach(function(tema) {
-      var autor = tema.author_name;
-      var contenido = tema.content;
-      var id = tema.id;
-      var respuestas = tema.responses_count;
-      plantillaFinal += plantilla.replace("__autor__", autor)
-        .replace("__contenido__", contenido)
-        .replace("__id__", id)
-        .replace("__respuestas__", respuestas);
-    });
-    $('#lista-temas').html(plantillaFinal);
+var cargarLista = function() {
+  $.getJSON(api.url, function(alumnas) {
+    alumnas.forEach(mostrarAlumnas);
   });
+};
+
+var mostrarAlumnas = function(alumna) {
+  var name = alumna.name;
+  var pLastName = alumna.pLastName;
+  var mLastName = alumna.mLastName;
+  var id = alumna.id;
+  plantillaFinal = plantilla.replace("__nombre__", name)
+  .replace("__pLastName__", pLastName)
+  .replace("__mLastName__", mLastName);
+
+    $('#asistentes').append(plantillaFinal);
 };
 
 
 var plantilla=
-  '<li>__name__<input type="checkbox"</input></li>' +
+  "<li>" +
+   "<input type='checkbox'>" + 
+   "__nombre__ " +
+   "</li>";
 
-
-
-
-var agregarEstudiante = function(e) {
-  e.preventDefault();
-  var autor = $('#autor').val();
-  var contenido = $('#contenido').val();
-  $.post(api.url, {
-    author_name: autor,
-    content: contenido
-  }, function(response){
-    $('#myModal').modal('hide');
-    cargarTemas();
-  });
-};
 
 $(document).ready(cargarPagina);
